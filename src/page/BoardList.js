360fb81cefe7d516e18c45c26732a658ad50e92a
soowan95 <ksw9565@gmail.com>
2023-11-09 14:50:48 +0900

    set BoardList

---
+++ src/page/BoardList.js
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+
+  useEffect(() => {
+    // axios.get("/api/board/list").then(({ data }) => setBoardList(data));
+  }, []);
+
+  return (
+    <Box>
+      <h1>게시물 목록</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList || <Spinner />}
+            {boardList &&
+              boardList.map((board) => (
+                <Tr>
+                  <Td>{board.id}</Td>
+                  <Td>{board.title}</Td>
+                  <Td>{board.writer}</Td>
+                  <Td>{board.inserted}</Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
