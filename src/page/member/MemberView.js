8009edeb2df339210c1aa1a86d4a74e4631ffdb3
soowan95 <ksw9565@gmail.com>
2023-11-13 09:38:54 +0900

    memberview page working...

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,7 +1,43 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 export function MemberView() {
+  const [member, setMember] = useState(null);
+
   const [param] = useSearchParams();
 
-  return <div>{param.get("id")} 회원 정보 보기</div>;
+  useEffect(() => {
+    axios
+      .get("/api/member?" + param.toString())
+      .then(({ data }) => setMember(data));
+  }, []);
+
+  if (member === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <h1>{member.id} 님 정보</h1>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input value={member.email} readOnly />
+      </FormControl>
+      <Button colorScheme="purple">수정</Button>
+      <Button colorScheme="red">삭제</Button>
+    </Box>
+  );
 }
