652e83cb760699c6f8a7ae884bfb4ff7235da850
soowan95 <ksw9565@gmail.com>
2023-11-14 15:21:59 +0900

    change navbar button follow the case-login

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,25 +1,44 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useContext } from "react";
+import { LoginContext } from "../App";
 
 export function NavBar() {
+  const { fetchLogin, login, isAuthenticated } = useContext(LoginContext);
+
+  const toast = useToast();
   const navigate = useNavigate();
 
   function handleLogout() {
     axios
       .post("/api/member/logout")
-      .then(() => console.log("success"))
-      .catch(() => console.log("fail"));
+      .then(() => {
+        toast({
+          description: "로그아웃 되었습니다.",
+          status: "info",
+        });
+        navigate("/");
+      })
+      .finally(() => fetchLogin());
   }
 
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
-      <Button onClick={() => navigate("/signup")}>signup</Button>
-      <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-      <Button onClick={() => navigate("/login")}>로그인</Button>
-      <Button onClick={handleLogout}>로그아웃</Button>
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/write")}>write</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/signup")}>signup</Button>
+      )}
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/login")}>로그인</Button>
+      )}
+      {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
     </Flex>
   );
 }
